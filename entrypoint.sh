80c63de34a27f1ee3bf473eb5980ad70ed6f63fe
Andre Lobato <andrefelipelobato@gmail.com>
2017-01-04 15:35:52 +1300

    Fix entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@ if [[ "$@" == *"/bin/sh -c"* ]]; then
     trap 'consul leave' TERM INT
     if [ -z "$CONSULDATA" ]; then export CONSULDATA="/tmp/consul-data";fi
     if [ -z "$CONSULDIR" ]; then export CONSULDIR="/consul";fi
-    if [ "$(ls -A $CONSUL)" ]; then
+    if [ "$(ls -A $CONSULDIR)" ]; then
         consul agent -data-dir=$CONSULDATA -config-dir=$CONSULDIR &
     fi
     $@ &
