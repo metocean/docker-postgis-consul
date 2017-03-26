083595ff0182738970e2ebdfd480c6b5fd7e8c98
Andre Lobato <andrefelipelobato@gmail.com>
2017-03-27 12:50:43 +1300

    Proper shutdown and fix mkdir init dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 if [[ "$@" == *"/bin/sh -c"* ]]; then
-    trap 'consul leave' TERM INT
+    trap 'consul leave; kill -TERM `cat /var/run/postgresql/9.5-main.pid`' TERM INT
     if [ -z "$CONSULDATA" ]; then export CONSULDATA="/tmp/consul-data";fi
     if [ -z "$CONSULDIR" ]; then export CONSULDIR="/consul";fi
     if [ "$(ls -A $CONSULDIR)" ]; then
         consul agent -data-dir=$CONSULDATA -config-dir=$CONSULDIR $CONSULOPTS &
+        CONSUL_PID=$!
+        
     fi
     $@ &
     PID=$!
     wait $PID
     trap - TERM INT
-    wait $PID
+    wait $CONSUL_PID
 else
     $@
 fi
