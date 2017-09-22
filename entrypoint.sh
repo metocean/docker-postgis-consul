100bbdc901266548bba56b77d02ecb444d2278f9
Andre Lobato <andrefelipelobato@gmail.com>
2017-09-22 16:44:19 +1200

    Consul leave after is all done

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,21 @@
 
 
 if [[ "$@" == *"/bin/sh -c"* ]]; then
-    trap 'consul leave; kill -TERM $PID' TERM INT
+    trap ' kill -TERM $PID' TERM INT
     if [ -z "$CONSULDATA" ]; then export CONSULDATA="/tmp/consul-data";fi
     if [ -z "$CONSULDIR" ]; then export CONSULDIR="/consul";fi
     if [ "$(ls -A $CONSULDIR)" ]; then
         consul agent -data-dir=$CONSULDATA -config-dir=$CONSULDIR $CONSULOPTS &
         CONSUL_PID=$!
+        CONSUL_LEAVE='consul leave'
     fi
     CMD=$(echo -e $@ | sed -e 's/\/bin\/sh -c //')
     $CMD &
     PID=$!
     wait $PID
     trap - TERM INT
+    wait $PID
+    $CONSUL_LEAVE
     wait $CONSUL_PID
 else
     $@
